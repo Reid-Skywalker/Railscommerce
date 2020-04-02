117845bc9c684814e53ef4727d640b220d200e8a
Reid Walker <Reid96@gmail.com>
2020-04-02 14:32:46 -0500

    Added catagory dropdown to product active admin

---
+++ app/admin/products.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+ActiveAdmin.register Product do
+  permit_params :name, :cost, :stock, :catagories_id, :image
+
+  form do |f|
+    f.semantic_errors *f.object.errors.keys
+
+    f.inputs 'Details' do
+      f.input :name
+      f.input :cost
+      f.input :stock
+      f.input :image
+      f.input :catagories_id, as: :select, collection: Catagory.all.collect { |category| [category.name, category.id] }
+    end
+
+    f.actions
+  end
+end
