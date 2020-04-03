ab5cd8e7d41f2d105cdc7277d4f091cb947b59ce
Reid Walker <Reid96@gmail.com>
2020-04-03 14:17:22 -0500

    added routing for products

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  resources :product, only: :show
+
+  get 'search', to: 'product#search', as: 'search'
+  get 'about', to: 'product#about', as: 'about'
+
+  root to: 'product#index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
