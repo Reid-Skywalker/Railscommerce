38391981613acbaa25505da45aaafb159b4727e1
Reid Walker <Reid96@gmail.com>
2020-04-16 14:03:09 -0500

    Modified customer and order models to add validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
 class Customer < ApplicationRecord
+  validates :name, :address, :phone, :email, presence: true
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class Order < ApplicationRecord
+  validates :quantity, :total, presence: true
+  validates :total, numericality: true
+  validates :quantity, numericality: { only_integer: true }
 end
