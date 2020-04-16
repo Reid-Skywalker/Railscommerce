8cf62ebaa9d3dc34f7682f212e90af5fa19a317f
Reid Walker <Reid96@gmail.com>
2020-04-16 14:03:59 -0500

    Modified product controller to add functionality for advanced search

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -2,14 +2,14 @@
 
 class ProductController < ApplicationController
   def index
-    @products = Product.order(:name)
+    @products = Product.order(:name).page params[:page]
   end
 
   def show
-    @product = Product.find(params[:id])
+    @product = Product.find(params[:id]).page params[:page]
   end
 
   def search
-    @products = Product.where('name LIKE ?', "%#{params[:search_term]}%")
+    @products = Product.joins('INNER JOIN catagories ON catagories.id = products.catagories_id').where('products.name LIKE ? AND catagories.id LIKE ?', "%#{params[:search_term]}%", "%#{params[:catagory]}%")
   end
 end
