aa118cc37c304110339d6b7a7ce26887442ac87f
Reid Walker <Reid96@gmail.com>
2020-04-21 15:26:14 -0500

    Added Cart functionality to product controller

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ProductController < ApplicationController
+  before_action :initailize_session
+  before_action :load_cart
   def index
     @products = Product.order(:name).page params[:page]
   end
@@ -11,5 +13,27 @@ class ProductController < ApplicationController
 
   def search
     @products = Product.joins('INNER JOIN catagories ON catagories.id = products.catagories_id').where('products.name LIKE ? AND catagories.id LIKE ?', "%#{params[:search_term]}%", "%#{params[:catagory]}%")
+  end
+
+  def add_to_cart
+    id = params[:id].to_i
+    session[:cart] << id unless session[:cart].include?(id)
+    redirect_to root_path
+  end
+
+  def remove_from_cart
+    id = params[:id].to_i
+    session[:cart].delete(id)
+    redirect_to root_path
+  end
+
+  private
+
+  def initailize_session
+    session[:cart] ||= []
+  end
+
+  def load_cart
+    @cart = Product.find(session[:cart])
   end
 end
