8167f6370e9f04cdd871a31413af636fcaaed93a
Reid Walker <Reid96@gmail.com>
2020-04-22 16:38:10 -0500

    Checkout controller create returning 500

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -15,7 +15,7 @@ class CheckoutController < ApplicationController
       product = Product.find(id)
       line_item = {
         name: product.name,
-        price: product.cost,
+        amount: product.cost,
         quantity: quantity,
         currency: 'cad'
       }
